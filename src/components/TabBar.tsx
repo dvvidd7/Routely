8da8d85b9a0d1af3752325512112cf3772281ed2
Retix <63494577+Davi1661@users.noreply.github.com>
2025-03-02 23:20:32 +0200

    AnimatedTabBar

---
+++ src/components/TabBar.tsx
@@ -0,0 +1,108 @@
+import { View, StyleSheet, LayoutChangeEvent } from 'react-native';
+import { useTheme } from '@react-navigation/native';
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import TabBarButton from './TabBarButton';
+import { useState } from 'react';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
+export function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
+  const { colors } = useTheme();
+
+  const [dimensions, setDimensions] = useState({ height: 20, width: 100 });
+
+  const buttonWidth = dimensions.width / state.routes.length;
+
+  const onTabbarLayout = (e: LayoutChangeEvent) => {
+    setDimensions({
+      height: e.nativeEvent.layout.height,
+      width: e.nativeEvent.layout.width,
+    });
+  };
+
+  const tabPositionX = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: tabPositionX.value }],
+    };
+  });
+
+  return (
+    <View onLayout={onTabbarLayout} style={styles.tabbar}>
+      <Animated.View
+        style={[
+          animatedStyle,
+          {
+            position: 'absolute',
+            backgroundColor: '#0384fc',
+            borderRadius: 30,
+            marginHorizontal: 25,
+            height: dimensions.height - 15,
+            width: buttonWidth - 75,
+            left: (buttonWidth - (buttonWidth - 25)) / 2, // Center the indicator
+          },
+        ]}
+      />
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+
+        // Ensure label is always a string, fallback to route.name if undefined
+        const label = (options.tabBarLabel ?? options.title ?? route.name) as string;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          tabPositionX.value = withSpring(buttonWidth * index, { duration: 1500 });
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TabBarButton
+            key={route.key}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            isFocused={isFocused}
+            routeName={route.name}
+            label={label}
+            color={isFocused ? '#FFF' : "#0384fc"}
+          />
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabbar: {
+    position: 'absolute',
+    bottom: 60,
+    left: 60,
+    right: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    borderRadius: 35,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+});
---
+++ src/components/TabBarButton.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react';
+import { Pressable, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import Animated, { interpolate, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+
+interface TabBarButtonProps {
+  onPress: () => void;
+  onLongPress: () => void;
+  isFocused: boolean;
+  routeName: string;
+  label: string;
+  color: string;
+}
+
+const TabBarButton: React.FC<TabBarButtonProps> = ({ onPress, onLongPress, isFocused, routeName, label, color }) => {
+  const icon: Record<string, (props: any) => JSX.Element> = {
+    index: (props) => <Feather name="home" size={24} {...props} />,
+    two: (props) => <Feather name="user" size={24} {...props} />,
+  };
+
+  const IconComponent = icon[routeName]; // Destructure to avoid repeated lookup
+
+  const scale = useSharedValue(0);
+
+  useEffect(() => {
+    scale.value = withSpring(isFocused ? 1 : 0, { duration: 350 });
+  }, [scale, isFocused]);
+
+  const animatedIconStyle = useAnimatedStyle(() => {
+    const scaleValue = interpolate(scale.value, [0, 1], [1, 1.2]);
+    const translateY = interpolate(scale.value, [0, 1], [0, 6]); // Adjusted translateY value to move down
+
+    return {
+      transform: [
+        { scale: scaleValue },
+        { translateY: translateY }
+      ]
+    };
+  });
+
+  const animatedTextStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(scale.value, [0, 1], [1, 0]);
+
+    return {
+      opacity,
+    };
+  });
+
+  return (
+    <Pressable onPress={onPress} onLongPress={onLongPress} style={styles.tabbarItem}>
+      <Animated.View style={animatedIconStyle}>
+        {IconComponent ? (
+          <IconComponent color={color} />
+        ) : null}
+      </Animated.View>
+      <Animated.Text style={[{ color, fontSize: 12 }, animatedTextStyle]}>
+        {label}
+      </Animated.Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabbarItem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+  },
+});
+
+export default TabBarButton;
