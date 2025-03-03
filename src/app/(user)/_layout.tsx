fe107affaf8f0a1c804c702da2dd11cce41df5ae
Eric Dragos <eric.dragos007@gmail.com>
2025-03-04 01:05:46 +0200

    User layout header styles (nimic)

--- src/app/(user)/_layout.tsx
+++ src/app/(user)/_layout.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
 import { TabBar } from '@/components/TabBar';
+import { StyleSheet } from 'react-native';
 
 const TabLayout = () => {
   return (
     <Tabs tabBar={(props) => <TabBar {...props} />}>
       <Tabs.Screen name="index" options={{ title: "Home", headerShown: false }} />
-      <Tabs.Screen name="two" options={{ title: "Profile" }} />
+      <Tabs.Screen name="two" options={{ title: "Profile", headerShown: true, headerTitleStyle:styles.headerTitleText, headerStyle:styles.headerBackground, headerShadowVisible: false }} />
     </Tabs>
   );
 };
+
+const styles = StyleSheet.create({
+  headerTitleText:{
+    fontSize: 40,
+  },
+  headerBackground:{
+    backgroundColor: 'white',
+  },
+});
 
 export default TabLayout;
